05ac7b1d380f91b1d37cefe3c4eef6b2d1ad110d
jase2026 <mailguard1@googlemail.com>
2026-04-21 18:45:14 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+// Service Worker for Pill Timer PWA
+const CACHE_NAME = 'pill-timer-v3';
+const FILES_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// Install: cache all files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+// Activate: clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: serve from cache, fall back to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
